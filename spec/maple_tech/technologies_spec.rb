d236a24510f954911df60d7e580518aad379bb0f
Muhammad Abdullah <dev@Devs-MacBook-Pro.local>
2024-01-02 20:52:24 +0500

    Technologies; Fixed rubocop offenses

--- spec/maple_tech/technologies_spec.rb
+++ spec/maple_tech/technologies_spec.rb
@@ -9,19 +9,19 @@ RSpec.describe(MapleTech::Technologies) do
 
   context 'technologies' do
     it 'should not have nil value' do
-      nil_value = MapleTech::Technologies.all.any? { |value| value.nil? }
+      nil_value = MapleTech::Technologies.all.any?(&:nil?)
       expect(nil_value).to(eql(false))
     end
 
     it 'should not have empty value' do
-      empty_value = MapleTech::Technologies.all.any? { |value| value.eql?('') }
+      empty_value = MapleTech::Technologies.all.any?(&:nil?)
       expect(empty_value).to(eql(false))
     end
 
     it 'should not have duplicate values' do
       uniq_technologies = MapleTech::Technologies.all.uniq(&:downcase)
       all_technologies = MapleTech::Technologies.all
-      expect(uniq_technologies).to match_array(all_technologies)
+      expect(uniq_technologies).to(match_array(all_technologies))
     end
 
     it 'should not have blank space as first or last character' do
@@ -29,7 +29,7 @@ RSpec.describe(MapleTech::Technologies) do
         value[0] == ' ' || value[-1] == ' '
       end
       all_technologies = MapleTech::Technologies.all
-      expect(technologies_with_empty_strings).to match_array(all_technologies)
+      expect(technologies_with_empty_strings).to(match_array(all_technologies))
     end
   end
 end
